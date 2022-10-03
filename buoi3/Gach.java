f28b3aa6422fafd2f5951aa3e790c0a22bfb769a
dangminhkhoi2212 <kkhoi600@gmail.com>
2022-10-03 21:54:52 +0700

    3/10/2022

--- buoi3/Gach.java
+++ buoi3/Gach.java
@@ -6,28 +6,31 @@ public class Gach {
 	private String maso;
 	private String mau;
 	private int cd,cr;
-	private static int n;
+	private int sl;
 	private long gia;
 	
 	public Gach(){
-		maso=mau="";
+		maso=new String();
+		mau=new String();
 		cd=cr=0;
 		gia=0;
-		n++;
+		sl=0;
 	}
-	public Gach(String maso1,String mau1,int cd1, int cr1) {
-		maso=maso1;
-		mau=mau1;
+	public Gach(String maso1, String mau1, int cd1, int cr1, int sl1, long gia1) {
+		maso=new String (maso1);
+		mau=new String(mau1);
 		cd=cd1;
 		cr=cr1;
-		n++;
+		sl=sl1;
+		gia=gia1;
 	}
 	public Gach(Gach g) {
-		maso=g.maso;
-		mau=g.mau;
+		maso=new String (g.maso);
+		mau=new String(g.mau);
 		cd=g.cd;
 		cr=g.cr;
-		n++;
+		sl=g.sl;
+		gia=g.gia;
 	}
 	public void nhap() {
 		Scanner sc=new Scanner(System.in);
@@ -38,44 +41,42 @@ public class Gach {
 		System.out.println("Nhap chieu dai va chieu rong vien gach:");
 		cd=sc.nextInt();
 		cr=sc.nextInt();
-		System.out.println("Nhap gia cua vien gach");
+		System.out.println("Nhap so luong vien gach:");
+		sl=sc.nextInt();
+		System.out.println("Nhap gia cua 1 hop:");
 		gia=sc.nextLong();
 	}
 	public void in() {
 		System.out.println("Ma so vien gach: "+maso+"\nMau cua vien gach: "+mau
-				+"\nChieu dai: "+cd+"\nChieu rong: "+cr+"\nGia: "+gia);
+				+"\nChieu dai: "+cd+"\nChieu rong: "+cr+"\nGia: "+gia+"\n---------o--------");
+		
 	}
 	public String toString() {
 		return ("Ma so vien gach: "+maso+"\nMau cua vien gach: "+mau
-				+"\nChieu dai: "+cd+"\nChieu rong: "+cr+"\nGia: "+gia);
+				+"\nChieu dai: "+cd+"\nChieu rong: "+cr+"\nGia: "+gia+"\n---------o--------");
 	}
 	public float giaBanLe() {
 		float gia_ban_le=(float)(gia+(float)gia*0.2);
-		return gia_ban_le/n;
+		return gia_ban_le/sl;
 	}
-	public float dienTich() {
-		return (float)(cd*cr)*n;
+	public int dienTich() {
+		return (cd*cr)*sl;
 	}
 	public float soLuongHop(int D, int N) {
-//		Code tham khao tren mang
-//		float slvDai=(float)D/cd;
-//		if((float)D/cd>0)
-//			slvDai++;
-//		float slvNgang=(float)N/cr;
-//		if((float)N/cr>0)
-//			slvNgang++;
-//		float tongSoVien=slvDai*slvNgang;
-//		float soHop=tongSoVien/n;
-//		if(tongSoVien%n>0)
-//				soHop++;
-//		return soHop;
-		
-		//Code tu viet
-		float soHop=(D*N)/dienTich();
-		if((D*N)%dienTich()>0)
-			soHop++;
-		return soHop;
-				
+		float slvDai=(float)D/cd;
+		if((float)D/cd>0)
+			slvDai++;
+		float slvNgang=(float)N/cr;
+		if((float)N/cr>0)
+			slvNgang++;
+		float tongSoVien=slvDai*slvNgang;
+		float soHop=tongSoVien/sl;
+		if(tongSoVien%sl>0)
+				soHop++;
+		return soHop;			
+	}
+	public long layGia() {
+		return gia;
 	}
 	
 }
--- buoi3/SDGach.java
+++ buoi3/SDGach.java
@@ -20,17 +20,44 @@ public class SDGach {
 			ds[i].in();			
 		}
 	}
+	public Gach chiPhiMin(Gach ds[]) {
+		float min=Float.MAX_VALUE;
+		Gach g=new Gach(ds[0]);
+		for(int i=0; i<ds.length; i++) {
+			float chiPHi=ds[i].giaBanLe()/ds[i].dienTich();
+			if(min > chiPHi) {
+				min=chiPHi;
+				g=new Gach(ds[i]);
+			}
+		}
+		return g;
+	}
+	public void tinhChiPhi(Gach ds[], int d, int r) {
+		for(int i=0; i<ds.length; i++) {
+			long gia=(long)ds[i].soLuongHop(d, r)*ds[i].layGia();
+			System.out.println("Gia cua hop gach thu "+(i+1)+" khi loat dien tich "+d+" x "+r+" la: "+gia);
+		}
+	}
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-//		Gach A=new Gach("123", "red", 10,20, 35000);
+		Gach A=new Gach("1", "red", 4,5,10, 30000);
+		Gach B=new Gach("2", "blue", 3,5,12, 25000);
+		Gach C=new Gach("3", "green", 3,6,15, 35000);
 //		System.out.println(A);
 		System.out.println("Nhap danh sach gach: ");
 		int n=sc.nextInt();
 		Gach ds[]= new Gach[n];
 		SDGach SD=new SDGach();
 		SD.khoiTaoDS(ds, n);
-		SD.nhapDS(ds);
+//		SD.nhapDS(ds);
+		ds[0]=new Gach(A);
+		ds[1]=new Gach(B);
+		ds[2]=new Gach(C);
 		SD.hienThiDS(ds);
+		
+		System.out.println("Loai gach co chi phi loat thap nhat:");
+		System.out.println(SD.chiPhiMin(ds));	
+		SD.tinhChiPhi(ds, 5, 20);
 		
 	}
 }
